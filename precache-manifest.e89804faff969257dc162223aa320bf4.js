a9bcb9bf9bfddee873b95ab8a6f188c7975bf91b
nickmwangemi <nickmwangemi@gmail.com>
2020-09-25 14:49:19 +0300

    Updates

---
+++ precache-manifest.e89804faff969257dc162223aa320bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3998a4f113d340056107727c0bd3c4a3",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "e4089dafac1fce28b318",
+    "url": "/expense-tracker-react/static/css/main.842e7c3b.chunk.css"
+  },
+  {
+    "revision": "c22895f4dca97ca6317f",
+    "url": "/expense-tracker-react/static/js/2.92176483.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.92176483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4089dafac1fce28b318",
+    "url": "/expense-tracker-react/static/js/main.f5c25b85.chunk.js"
+  },
+  {
+    "revision": "c67e8cd613f94f5cd430",
+    "url": "/expense-tracker-react/static/js/runtime-main.71fabd8b.js"
+  }
+]);
